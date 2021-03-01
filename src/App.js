e5685624f3ac0f0abed4ef02feda7e7159b720df
Marta Małachowicz <marta@exefs-macbook-pro.home>
2021-03-01 13:05:16 +0100

    redux persist

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React from "react";
-import { createStore, applyMiddleware } from "redux";
+
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { ChakraProvider } from "@chakra-ui/react";
 import rootReducer from "./rootReducer";
 
 import "./App.css";
 import Users from "./pages/users/containers/Users";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer),
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose,
+  store = createStore(
+    persistedReducer,
+    /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+  );
+
+let persistor = persistStore(store);
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <ChakraProvider>
-          <Users />
-        </ChakraProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <ChakraProvider>
+            <Users />
+          </ChakraProvider>
+        </PersistGate>
       </Provider>
     </div>
   );
