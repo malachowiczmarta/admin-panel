8821160c22452c74fb0f83ba771332afcfbca550
Marta Małachowicz <marta@exefs-macbook-pro.home>
2021-03-02 23:51:02 +0100

    readme

--- src/store/reducers/ui.js
+++ src/store/reducers/ui.js
@@ -1,25 +1,8 @@
-// const ALERT = "ui/FAV_ALERT";
-// const ALERT_TYPE = "ui/FAV_ALERT_TYPE";
 const SHOW_POPUP = "ui/SHOW_POPUP";
 
 const INITIAL_STATE = {
-//   showAlert: false,
-//   alertType: "",
   showPopup: false,
 };
-
-// export function alert() {
-//   return {
-//     type: ALERT,
-//   };
-// }
-
-// export function setAlertType(type) {
-//   return {
-//     type: ALERT_TYPE,
-//     payload: type,
-//   };
-// }
 
 export function setPopup() {
   return {
@@ -29,10 +12,6 @@ export function setPopup() {
 
 function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    // case ALERT:
-    //   return { ...state, showAlert: !state.showAlert };
-    // case ALERT_TYPE:
-    //   return { ...state, alertType: action.payload };
     case SHOW_POPUP:
       return { ...state, showPopup: !state.showPopup };
     default:
